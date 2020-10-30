257d1f8d3985f684ef4ff5c8dc1a8909ae6dd183
josecruzio <josericardo.developer@gmail.com>
2020-10-30 19:25:36 +0000

    Filling the customer page, template changes and url routing.

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+
+
 # Create your views here.
 
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
-    
+
     total_customers = customers.count()
     total_orders = orders.count()
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
 
-    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
+               'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
     return render(request, 'accounts/dashboard.html', context)
 
-def products(request):
-        products = Product.objects.all()
-        return render(request, 'accounts/products.html', {'productList': products})
 
-def customer(request):
-     return render(request, 'accounts/customer.html')
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'productList': products})
+
+
+def customer(request, customerPk):
+    customer = Customer.objects.get(id=customerPk)
+    orders = customer.order_set.all()  # query the orders
+
+    context = {'customer': customer, 'orders': orders}
+    return render(request, 'accounts/customer.html', context)
